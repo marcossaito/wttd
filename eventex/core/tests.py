f665d235b0bb867f73bf33d10a160a0cb74d0dae
Marcos Saito <m.saito@consultoriasigma.com.br>
2014-06-30 08:39:09 -0300

    Projeto ate dia 30

--- eventex/core/tests.py
+++ eventex/core/tests.py
@@ -8,9 +8,9 @@ Replace this with more appropriate tests for your application.
 from django.test import TestCase
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+class HomepageTest(TestCase):
+    def test_get(self):
+    	'TESTE GET / retorna codigo 200'
+        response = self.client.get('/')
+        self.assertEqual(200, response.status_code)
+        self.assertTemplateUsed(response, 'index.html')
--- eventex/settings.py
+++ eventex/settings.py
@@ -14,15 +14,8 @@ ADMINS = (
 MANAGERS = ADMINS
 
 DATABASES = {
-    'default': dj_database_url.config(default="sqlite:///"+ PROJECT_DIR.child('database.db')){
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': PROJECT_DIR.child('database.db'),                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+    'default': dj_database_url.config(default="sqlite:///"+ PROJECT_DIR.child('database.db'))
     }
-}
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
